4a057013a2bb2d236e49643c5079bed55a671b80
Sol Cruz <solacruz@fb.com>
2018-07-05 18:35:03 -0700

    Relative Date Added

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdaptor.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdaptor.java
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,10 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetAdaptor extends RecyclerView.Adapter<TweetAdaptor.ViewHolder>{
 
@@ -24,6 +28,24 @@ public class TweetAdaptor extends RecyclerView.Adapter<TweetAdaptor.ViewHolder>{
     public TweetAdaptor(List<Tweet> tweets){
         mTweet=tweets;
 
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 
     //for each row, inflate the layout and cache references in the ViewHolder
@@ -50,6 +72,7 @@ public class TweetAdaptor extends RecyclerView.Adapter<TweetAdaptor.ViewHolder>{
         //give the views the data in the correct order/populate the views
         holder.tvUsername.setText(tweet.user.name);
         holder.tvBody.setText(tweet.body);
+        holder.tvRelDate.setText(getRelativeTimeAgo(tweet.createdAt));
 
         Glide.with(context).load(tweet.user.profileImageURL).into(holder.ivProfileImage);
     }
@@ -64,6 +87,7 @@ public class TweetAdaptor extends RecyclerView.Adapter<TweetAdaptor.ViewHolder>{
         public ImageView ivProfileImage;
         public TextView tvUsername;
         public TextView tvBody;
+        public TextView tvRelDate;
 
 
         public ViewHolder (View itemView){
@@ -73,6 +97,7 @@ public class TweetAdaptor extends RecyclerView.Adapter<TweetAdaptor.ViewHolder>{
             ivProfileImage=itemView.findViewById(R.id.ivProfileImage);
             tvUsername=itemView.findViewById(R.id.tvUsername);
             tvBody=itemView.findViewById(R.id.tvBody);
+            tvRelDate=itemView.findViewById(R.id.tvRelDate);
         }
     }
 }
